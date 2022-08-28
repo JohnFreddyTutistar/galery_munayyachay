fedcccd3049923b641ee7ca7faf8b350199b2004
JohnFreddyTutistar <johnfre.157@gmail.com>
2022-08-27 22:54:05 -0500

    create new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GaleryImgComponent } from './galery/galery-img/galery-img.component';
+import { ImgComponentComponent } from './imgComponent/imgComponent.component';
+
+const routes: Routes = [
+  { 
+    path: 'compGalery', 
+    component: ImgComponentComponent 
+  }
+];
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-    GaleryImgComponent
-  ],
+    GaleryImgComponent,
+    ImgComponentComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/imgComponent/imgComponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imgComponent',
+  templateUrl: './imgComponent.component.html',
+  styleUrls: ['./imgComponent.component.css']
+})
+export class ImgComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
